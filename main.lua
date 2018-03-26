d93f6a5216ee3e85d9556cfd730f05cda37aa21c
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-03-26 14:51:13 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.Lua
+--
+-- tells you if you if you get a discount
+--
+--
+-- Created by: Fawaz Mezher
+-- Created on: March 2018 
+-----------------------------------------------------------------------------------------
+
+-- instrcution
+local InstructionTextField = display.newText( "Enter age and the day of the week.", display.contentCenterX - 400, display.contentCenterY - 500, native.systemFont, 70 )
+
+local ageTextField = native.newTextField( display.contentCenterX - 500, display.contentCenterY - 200, 750, 150 )
+ageTextField.id = "age textField"
+
+local weekdayTextField = native.newTextField( display.contentCenterX - 500, display.contentCenterY - 00, 750, 150 )
+weekdayTextField.id = "weekday textField"
+
+local ageTitleTextField = display.newText( "age", display.contentCenterX - 00, display.contentCenterY - 200, native.systemFont, 100 )
+ageTitleTextField:setFillColor( 99, 10, 88 )
+
+local weekdayTitleTextField = display.newText( "day of the week", display.contentCenterX + 300, display.contentCenterY - 00, native.systemFont, 100 )
+weekdayTitleTextField:setFillColor( 99, 10, 88 )
+
+local calculateButton = display.newImageRect( "./assets/sprites/enterButton.png", 406, 157 )
+calculateButton.x = display.contentCenterX +800
+calculateButton.y = display.contentCenterY +500
+calculateButton.id = "calculate button"
